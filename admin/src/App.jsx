7bb6a93b6f84a1ee4a1eaa4a968cd9bd0c6352fb
Veluri Yuvraj Reddy <147172115+veluriyuvrajreddy@users.noreply.github.com>
2024-07-11 21:04:52 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
-  const url = "http://localhost:4000";
+  const url = "https://food-delivery-backend-16k7.onrender.com";
   return (
     <div>
       <ToastContainer />
